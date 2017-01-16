1010c1e6d7a7889c6b49d9164539e3c82cab1d80
tomkee <tiomek93@gmail.com>
2017-01-16 23:21:16 +0100

    Dodałem obsługe Kafki. Działa, ale cassandra ssie.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,16 @@
 import bolts.*;
 import org.apache.storm.Config;
 import org.apache.storm.LocalCluster;
+import org.apache.storm.kafka.*;
+import org.apache.storm.spout.SchemeAsMultiScheme;
 import org.apache.storm.topology.TopologyBuilder;
+import org.apache.storm.tuple.Fields;
 import org.apache.storm.utils.Utils;
 import spouts.DummySpout;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 /**
  * Created by tom on 13.01.17.
@@ -14,19 +18,22 @@ import java.util.Map;
 
 public class Main {
 
-//    static IRichBolt cassandraAirportWriter = new CassandraWriterBolt(
-//            async(
-//                    simpleQuery("UPDATE airports SET number = number + 1 WHERE airport_name=?;")
-//                    .with(all())));
-//
-
-
 
     public static void main(String[] args) throws Exception {
+
+        String zkIp = "127.0.0.1";
+        String zookeeperHost = zkIp +":2181";
+
+        ZkHosts zkHosts = new ZkHosts(zookeeperHost);
+
+        SpoutConfig kafkaConfig = new SpoutConfig(zkHosts, "storm-test", "", "storm");
+
+        kafkaConfig.scheme = new SchemeAsMultiScheme(new StringScheme());
+        KafkaSpout kafkaSpout = new KafkaSpout(kafkaConfig);
         TopologyBuilder builder = new TopologyBuilder();
 
-        builder.setSpout("source", new DummySpout(), 1);
-        builder.setBolt("split", new SplitBolt(), 1).shuffleGrouping("source");
+        builder.setSpout("source", kafkaSpout, 2);
+        builder.setBolt("split", new SplitBolt(), 2).shuffleGrouping("source");
 
         // ad1
         builder.setBolt("filterAirports", new FilterAirportsBolt(), 1).shuffleGrouping("split");
@@ -47,12 +54,11 @@ public class Main {
 
         Config conf = new Config();
         conf.setDebug(true);
+//        conf.put(Config.TOPOLOGY_DEBUG, false);
 
 
         LocalCluster cluster = new LocalCluster();
         cluster.submitTopology("test", conf, builder.createTopology());
-        Utils.sleep(10000);
-        cluster.killTopology("test");
-        cluster.shutdown();
+
     }
 }
